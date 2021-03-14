67b03fe51385ae853c9a5451529ef6fc14f52aaf
Josh Durham <joshdurham5599@gmail.com>
2021-03-14 13:26:34 -0500

    ContributorGenerator pretty much done, using map and list of usernames, hard code out 1 exception to rule

--- ContributorGenerator.hpp
+++ ContributorGenerator.hpp
@@ -7,6 +7,7 @@ public:
     virtual std::string gen_str() override;
     virtual void store_str(const std::string& str) override;
     virtual std::pair<int, int> find_range(const std::string& str) override;
+    virtual void store_to_file(const std::string& filename) override;
 private:
     enum class ContributorType {
         ID_SPECIFIED, IP, CONVERSION_SCRIPT, TEMPLATE_NAMESPACE,
@@ -17,11 +18,11 @@ private:
         char nums[4];
     };
     static std::map<int, std::string> id_to_name;
-    static std::queue<std::string> user_names;
-    static std::queue<int> user_ids;
-    static std::queue<ContributorType> types;
-    static std::queue<IP> ips;
-    static std::queue<std::string> misc;
+    static std::deque<std::string> user_names;
+    static std::deque<int> user_ids;
+    static std::deque<ContributorType> types;
+    static std::deque<IP> ips;
+    static std::deque<std::string> misc;
     static const std::string start_str;
     static const std::string end_str;
 };
--- StringGenerator.hpp
+++ StringGenerator.hpp
@@ -10,6 +10,6 @@ public:
     // returns start and len of first substring it can process
     // pair returns -1, 0 if no match
     virtual std::pair<int, int> find_range(const std::string& str) = 0;
-    //virtual void store_to_file(const std::string& filename) = 0;
+    virtual void store_to_file(const std::string& filename) = 0;
     //virtual void load_from_file(const std::string& filename) = 0;
 }; 
